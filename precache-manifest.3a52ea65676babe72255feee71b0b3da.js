b4d874c59a60e7e18f2dbb5dfab2a2ebbda2b544
Akram <akram@Akrams-MacBook-Pro.local>
2020-05-29 19:23:12 +0100

    Updates

---
+++ precache-manifest.3a52ea65676babe72255feee71b0b3da.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e89dd89221a6b0588dd81a9fd97a084d",
+    "url": "/poemMaker/index.html"
+  },
+  {
+    "revision": "0e1e997d644363298b85",
+    "url": "/poemMaker/static/css/main.bd0f7cd0.chunk.css"
+  },
+  {
+    "revision": "08bb98cfdc2dc65417eb",
+    "url": "/poemMaker/static/js/2.998940eb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/poemMaker/static/js/2.998940eb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0e1e997d644363298b85",
+    "url": "/poemMaker/static/js/main.2f9f91ec.chunk.js"
+  },
+  {
+    "revision": "6a65a24ab8af3d0e5ebf",
+    "url": "/poemMaker/static/js/runtime-main.65601da1.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/poemMaker/static/media/logo.5d5d9eef.svg"
+  }
+]);
